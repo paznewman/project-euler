9d47db19b0a4529b1e7ad8f60e1afaef1c11738c
paznewman <newman.paz@gmail.com>
2018-12-01 13:48:59 +0200

    Create 0009_special_Pythagorean_triplet.py
    result 31875000

---
+++ Python/0009_special_Pythagorean_triplet.py
@@ -0,0 +1,33 @@
+""" A Pythagorean triplet is a set of three natural numbers, a < b < c,
+    for which a^2 + b^2 = c^2
+
+    For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
+"""
+
+def sum_triplets (n):
+    """ generates all triplets of integers which sum up to _n_. """
+
+    def sum_pairs (n):
+        """ generates all pairs of integers which sum up to _n_. """
+        a = n-1
+        while a >= 1:
+            yield [a, n-a]
+            a-=1
+            
+    a = n-2
+    while a >= 2:
+        for pair in sum_pairs(n-a):
+            yield [a] + pair
+        a-=1
+
+
+for triplet in sum_triplets(1000):
+    c = triplet[0]
+    b = triplet[1]
+    a = triplet[2]
+    if a*a + b*b == c*c:
+        print(a*b*c)
+        break
